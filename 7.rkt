0e881fd1a790fc63282db2262025f97364aa3d59
Stephen Chang <stchang216@gmail.com>
2011-08-27 05:49:47 -0400

    add 7.rkt

--- 7.rkt
+++ 7.rkt
@@ -1,13 +1,43 @@
 #lang racket
 
 
+;; not working -- run and see
+
+
+(define (read-chunk p)
+  (printf "chunk: ")
+  (define length-bytes (reverse (bytes->list (read-bytes 4 p))))
+  (for-each displayln length-bytes)
+  (define length (+ (first length-bytes)
+                    (* (second length-bytes) 255)
+                    (* (third length-bytes) 255 255)
+                    (* (fourth length-bytes) 255 255 255)))
+  (printf "~a bytes, " length)
+  (define type-bytes (bytes->list (read-bytes 4 p)))
+  (define type (apply string (map integer->char type-bytes)))
+  (printf "type: ~a\n" type)
+  (define data-bytes (bytes->list (read-bytes length p)))
+  (read-bytes 4 p))
+  
+
 (define p (open-input-file "7/oxygen.png" #:mode 'binary))
 
-(let loop ([count 0])
-  (let ([b (read-byte p)])
-    (unless (eof-object? b)
-      (when (zero? b)
-        (printf "~a\n" count))
-      (loop (add1 count)))))
-    
+
+;; A PNG file starts with an 8-byte signature. 
+;; The hexadecimal byte values are 89 50 4E 47 0D 0A 1A 0A; 
+;; the decimal values are 137 80 78 71 13 10 26 10
+(define header (read-bytes 8 p))
+(let loop ()
+  (unless (eof-object? (peek-bytes 1 0 p))
+    (read-chunk p)
+    (loop)))
+
+
 (close-input-port p)
+
+#;(let loop ([bs bs])
+  (when (= (first bs) (second bs) (third bs))
+    (printf "~a\n" (integer->char (first bs))))
+  (unless (= 3 (length bs))
+    (loop (cdr bs))))
+      
